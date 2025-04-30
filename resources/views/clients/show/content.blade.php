2f5b68543f44eb73707cb7fa1fcf8fbed0751c92
GUIFFROY Romain <rguiffroy@decima.fr>
2025-04-30 14:30:28 +0200

    ajout de la typo

--- resources/views/clients/show/content.blade.php
+++ resources/views/clients/show/content.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('head')
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:wght@400;500;600;700&display=swap" rel="stylesheet">
 @endsection
 
 @section('content')

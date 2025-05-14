0ccdf5f7c45962115598fa813fdb347878363aba
GUIFFROY Romain <rguiffroy@decima.fr>
2025-05-14 10:52:09 +0200

    vraie version

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -9,16 +9,13 @@ class Service extends Model
     protected $fillable = [
         'client_id',
         'site_id',
-        'nom',           // Ajout de ce champ
         'configuration',
         'lignes',
-        'services',      // Ajout de ce champ
     ];
 
     protected $casts = [
         'configuration' => 'array',
         'lignes' => 'array',
-        'services' => 'array', // Ajout de ce cast
     ];
 
     public function client()
